a6903e3b3ef94f6c00a834d2a9c03888c6b1f52b
navsa <navsa@LAPTOP-E4EJHEPF>
2019-09-12 12:45:37 -0400

    initial

--- src/BlazeDempTest.java
+++ src/BlazeDempTest.java
@@ -3,20 +3,50 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDempTest {
 
 	@Before
 	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("THIS IS SETUP");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		Thread.sleep(5000);
+		driver.close();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDepartureCities() {
+		// 1. Enter some value into the text box
+			
+				WebElement inputBox = driver.findElement(By.id("Choose Your Departure City"));
+				
+				inputBox.sendKeys("");
+				// 2. Push the button
+				// - GET the button
+				WebElement showMessageButton = driver.findElement(
+						By.cssSelector("form#get-input button"));
+				// - PUSH the button
+				showMessageButton.click();
+				
+				
+				
+				// ----------------------------------------------
+				// 3. Get the actual output from the screen
+				WebElement outputBox = driver.findElement(By.id(""));
+				String actualOutput = outputBox.getText();
+				
+				// 4. Check if expected output == actual output
+				assertEquals("", actualOutput);
 	}
 
 }
